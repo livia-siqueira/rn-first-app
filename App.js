939e7ffdd20186231d2e9861f50bfaf8cae5734c
Lívia Siqueira <livia.siqueira12@outlook.com>
2021-12-02 16:26:14 -0300

    feat: first app in react native

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [output, setOutPut] = useState('Open up App.js to start working on your app!')
+
+
+  const handleClick = () => {
+    setOutPut((output) => {
+     return  output === 'Hi, sucess' ?  'Open up App.js to start working on your app!' :  'Hi, sucess'
+    } )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{output}</Text>
+      <Button onPress={handleClick} title="Click me!"/>
       <StatusBar style="auto" />
     </View>
   );
